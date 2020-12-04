5b840647f0f20f5e01175f4b9d4cf3e8f77db427
Ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-12-04 15:27:39 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_COPY_FILES += \
 
 # FOD Permissions
 PRODUCT_COPY_FILES += \
-    vendor/corvus/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    vendor/aosip/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
